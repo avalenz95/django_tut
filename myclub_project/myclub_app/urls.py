9641bd0b5c6976b1e1396ed6d58e783c00564273
ablades <audaris@gmail.com>
2019-11-05 15:04:47 -0800

    update tut

--- myclub_project/myclub_app/urls.py
+++ myclub_project/myclub_app/urls.py
@@ -1,4 +1,4 @@
-"""myclub_project URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import include, path
+from . import views
+from myclub_app.views import ClubView
 
-
-#imports all urlpatterns through apps
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('clubs/', include('myclub_app.urls'))
+    # clubs/
+    path('', views.hello_world),
+    # clubs/class/
+    path('class/', ClubView.as_view()),
+
 ]
--- myclub_project/myclub_app/views.py
+++ myclub_project/myclub_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from django.views.generic.list import ListView
+#Import event class
+from myclub_app.models import Event
 
 # Create your views here.
+
+
+#function based view
+def hello_world(request):
+    return HttpResponse("<h1>Hello World</h1>")
+
+
+
+#class based view
+class ClubView(ListView):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Hello, World!')
+
+class EventView(View):
+    model = Event
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    pass
